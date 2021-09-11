44280d64b173861e71778ae201b58417a99ba379
Nichm0617 <nicholasmataczynski0617@gmail.com>
2021-09-10 23:55:00 -0400

    Update Lab1.py

--- Lab 1/Lab1.py
+++ Lab 1/Lab1.py
@@ -21,6 +21,14 @@ def cost(b, m, years, prices):
 
     return endCost
 
+#cost algorithm used for stocashtic. Makes use of dot.
+def  cost_function_theta(theta,y,x):
+    hypothesese = x.dot(theta)
+    summing = np.sum(np.square(hypothesese - y))
+    total = (1/2 * len(y)) * summing
+    return total
+
+
 #This is gradient descent using the batch method. Here we find the derivates of b and m.
 def batch_gradient_descent(b, m, years, prices, alpha, epochs):
     bTraining = b
@@ -40,6 +48,26 @@ def batch_gradient_descent(b, m, years, prices, alpha, epochs):
 
     return bTraining, mTraining
 
+
+#The stocashtic version of gradient descent
+def stocashtic_gradient_descent(theta, x, y, iters, learningRate):
+    allCosts = np.zeros(iters)
+
+    for iter in range(iters):
+        cost = 0.0
+        for i in range(len(y)):
+            random_index = np.random.randint(0, len(y))
+            yMid = y[random_index,0].reshape(1,1)
+            xMid = x[random_index,0].reshape(1,x.shape[1])
+            hypothesis = np.dot(xMid,theta)
+
+            theta -= (1/len(y)) * (xMid.T.dot((hypothesis - yMid))) * learningRate
+            cost = cost + (cost_function_theta(theta,yMid,xMid))
+        allCosts[iter] = cost
+    finalTheta = theta
+
+    return allCosts, finalTheta
+
 #main function that will read the data, run the gradient descents, then ask you for a year to predict the price of gold in
 def main():
     workbook = pd.read_csv('annual_csv.csv', usecols = ['Price','Date'])
@@ -58,6 +86,7 @@ def main():
 
     endCost = cost(bTrained, mTrained, yearList, priceList)
 
+    print("Batch Gradient Descent:")
     print("Weight of b: " + str(bTrained))
     print("Weight of m: " + str(mTrained))
     print("Loss: " + str(endCost))
@@ -73,6 +102,22 @@ def main():
     #         print("The predicted price is: " + str(guess))
     #     else:
     #         print("Please enter a year, or end to stop.")
+
+
+    #populate arrays
+    theta = np.random.randn(1,2)
+    x = np.random.rand(70,1)
+    y = np.random.randn(70,1)
+    for num in range(len(yearList)):
+        x[num][0] = yearList[num]
+        y[num][0] = priceList[num]
+
+    allCosts, finalTheta = stocashtic_gradient_descent(theta, x, y, 50, 0.001)
+
+    print("Stocashtic Gradient Descent:")
+    print("Weight of b: " + str(finalTheta[0][0]))
+    print("Weight of m: " + str(finalTheta[0][1]))
+    print("Loss: " + str(allCosts[-1]))
 
 
 if __name__ == '__main__':
